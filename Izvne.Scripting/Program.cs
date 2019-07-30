9bc42384723ef4ed0511bccc40200ef3a63d1bcb
vkugelevics <vladislavs.kugelevics@28stone.com>
2019-07-30 22:58:10 +0300

    scripting project

--- Izvne.Scripting/Program.cs
+++ Izvne.Scripting/Program.cs
@@ -1,30 +1,146 @@
-﻿namespace Izvne.Scripting
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.PhotosLibrary.v1;
+using Google.Apis.PhotosLibrary.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using LINQPad;
+using Newtonsoft.Json;
+
+namespace Izvne.Scripting
 {
     internal class Program
     {
+        private static bool _uploadPhotos = false;
+        private static bool _getFinalUrls = true;
+        private static bool changePostUrls = true;
+        private const string AaUrl = "https://get.google.com/albumarchive/101096522944241604628";
+
+        private const string AuthCookie =
+            "CONSENT=YES+LV.ru+20160410-02-0; S=billing-ui-v3=QWBYsxsPARdklJOhGWMbcxbZMr06-zUJ:billing-ui-v3-efe=QWBYsxsPARdklJOhGWMbcxbZMr06-zUJ; OTZ=5018887_44_48_123900_44_436380; SID=mgdAedstvVNuVCUPVRtSn1JpsfPNWz4oA_8ib-1oYh-bKtG3nhqaIGNAPGYNg5WW5_dAtA.; HSID=AIdPjjStOEoCL2EGL; SSID=A5tQLZmaHBfKhhaSA; APISID=H2tFYZEXFPocDCQP/A5zAu3Bri3ehTJ3Kk; SAPISID=wAVJ8HmKDqRlSzB8/Ah9TTFkt3MmQthi9F; SEARCH_SAMESITE=CgQIu40B; 1P_JAR=2019-7-30-15; NID=188=SjnoUPTJVg6mfp7YPt9_qUcIL6Ef5DK74qF0Q4n-8gb2Vz4maBTypmMCh7sVi4sWsu26s25kTIZFLGqGUbwEQByjhXMl4HRfo_vZcv_jzH53R5uK-4ccHsfvi9C6XpnyK-q96P_P6xvFfWeVAlhkq1snlSFVONnr1pnVLq__idmzCwIyZWsyeNBPD3bsNMPtXLx-kl_wUDtpOUsNq-RBjOjRouzBGOjkZcY1PAW8jU1LnQTc62A8Z9Fc12cP2pIt4eLHvUgWqXHSW6w5iMk6t5n79QxPn7ce5OMG34vsJvl1cpDwycR_52Lm8MNRmdXTybdtl5MM7Gpw2cByhnUF2-oYpo8JGQ; SIDCC=AN0-TYsvt11riFowOC9ifjxslSsjZFNF2o0kx6MSLrNxN4gSC1NIFfezMc_Od7SJocv2FZNdpRM";
+
+        private const string AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+        private const string ClientId = "276048709422-3ov23ibvjjlv28v4f9hmsrbnb4l4igl7.apps.googleusercontent.com";
+        private const string ClientSecret = "KKBt9jn6cqBQxqCOF2dTKXkl";
+        private const string TokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
+        private const string UserInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
+
+
+        private static string[] GetFinalUrls(string albumname)
+        {
+            string res;
+            using (var wcForAlbumArc = new WebClient())
+            {
+                wcForAlbumArc.Headers.Add("Cookie", AuthCookie);
+                res = Encoding.UTF8.GetString(wcForAlbumArc.DownloadData(AaUrl));
+
+                var match = Regex.Split(res, "\n").Where(l => l.Contains($",\"{albumname}\""))
+                                 .Select(str => str.Trim(','))
+                                 .FirstOrDefault();
+
+                var albumUrl = $"{AaUrl}/album/{match?.Split('"')[5]}";
+
+                res = Encoding.UTF8.GetString(wcForAlbumArc.DownloadData(albumUrl));
+            }
+
+            var lines = Regex.Split(res, "\n")
+                             .FirstOrDefault(l => l.Contains("googleusercontent") && l.Contains("=w"));
+            var results = new List<string>();
+            foreach (Match m in Regex.Matches(lines, @"img src=""([^""=]+)"))
+            {
+                results.Add(m.Groups[1].Value.Dump());
+            }
+
+            return results.ToArray();
+        }
+
+        public static string[] GetImagesInFile(string path)
+        {
+            var result = new List<string>();
+            var lines = File.ReadAllLines(path);
+
+            {
+                foreach (var line in lines)
+                {
+                    var pattern = @"\!\[[^]]*\]+\(([^)]+)\)";
+
+                    var matches = Regex.Match(line, pattern);
+                    using (var wc = new WebClient())
+                    {
+                        if (matches.Success)
+                        {
+                            var val = matches.Groups[1].Value;
+                            result.Add(val);
+                        }
+                    }
+
+                    ;
+                }
+
+                return result.ToArray();
+            }
+        }
+
+        public string GetPermUrl(string photoSharingUrl)
+        {
+            var wc = new WebClient();
+            //wc.DownloadString();
+            var col = new NameValueCollection();
+            col.Add("googlephotos", photoSharingUrl);
+            wc.Headers.Add("Referer", "https://ctrlq.org/google/photos/");
+
+            var r = wc.UploadValues("https://ctrlq.org/google/photos/og.php", col);
+            var json = Encoding.UTF8.GetString(r).Dump();
+            var res = (string) JsonConvert.DeserializeObject<dynamic>(json).image;
+            return res;
+        }
+
         public static void Main(string[] args)
         {
-//config
+            var (service, accessToken) = PrepareService();
 
-            var uploadPhotos = false;
+            //config
+            _uploadPhotos = true;
+            var doAuth = true;
 
 
+            var postPath =
+                @"D:\Docs\Source\Izvne.com\Izvne.com\input\post\Шотландия\Шотландия — 3\Шотландия — 3, день первый, неприятность номер один.md";
+
+            var allPosts = Directory.GetFiles(@"D:\Docs\Source\Izvne.com\Izvne.com\input\post\", "*.md",
+                                              SearchOption.AllDirectories);
+
+            foreach (var onePostPath in allPosts)
+            {
+                ProcessOnePost(onePostPath, _uploadPhotos, service, accessToken);
+            }
+        }
+
+        private static (PhotosLibraryService, string) PrepareService()
+        {
             UserCredential credential =
                 Util.Cache(() =>
                            {
                                return GoogleWebAuthorizationBroker.AuthorizeAsync(
                                                                                   new ClientSecrets
                                                                                   {
-                                                                                      ClientId = clientId,
-                                                                                      ClientSecret = clientSecret
+                                                                                      ClientId = ClientId,
+                                                                                      ClientSecret = ClientSecret
                                                                                   },
                                                                                   new[]
                                                                                   {
                                                                                       PhotosLibraryService
                                                                                           .Scope.Photoslibrary,
-                                                                                      PhotosLibraryService
-                                                                                          .Scope
-                                                                                          .PhotoslibrarySharing
+                                                                                      PhotosLibraryService.Scope
+                                                                                                          .PhotoslibrarySharing
                                                                                   },
                                                                                   "user",
                                                                                   CancellationToken.None,
@@ -36,109 +152,82 @@
             var serv = new PhotosLibraryService(new BaseClientService.Initializer
                                                 {
                                                     HttpClientInitializer = credential
-                                                    //		ApplicationName = "BlogUploader",
-                                                    //		ApiKey = "ya29.GltVB1r857QYafID4q7LdNtQCeCKMgW5GsB6_qz2Xf-p2ut8UT7i5uQ_5wEb_olZOufSBNTyLLim-5faEWq2l4aFQzAOZe1jPz4zCpY_hsChyYUAGQr2gt8NAR7H"		,
                                                 });
+            return (serv, credential.Token.AccessToken);
+        }
 
-            uploadPhotos = true;
-
-            var postPath =
-                @"D:\Docs\Source\Izvne.com\Izvne.com\input\post\Шотландия\Шотландия — 3\Шотландия — 3, день первый, неприятность номер один.md";
-
+        private static void ProcessOnePost(string postPath, PhotosLibraryService photosService,
+                                           string accessToken)
+        {
             var postPathName = Path.GetFileNameWithoutExtension(postPath);
             var imageFilenames = GetImagesInFile(postPath);
-            if (uploadPhotos)
+            if (_uploadPhotos)
             {
-                //var als=serv.Albums.List().Execute();
-
-
-                var body = new CreateAlbumRequest();
-                body.Album = new Album();
-                body.Album.Title = postPathName;
-                //body.Album.ShareInfo = new ShareInfo() { SharedAlbumOptions = new SharedAlbumOptions() {IsCollaborative=false,IsCommentable=false}};
-                var reponse = serv.Albums.Create(body).Execute().Dump();
-
-
-                var body2 = new BatchCreateMediaItemsRequest()
-                            {
-                                AlbumId = reponse.Id,
-                                NewMediaItems = new List<Google.Apis.PhotosLibrary.v1.Data.NewMediaItem>()
-                            };
-
-                ShareAlbumRequest sar = new ShareAlbumRequest()
-                                        {
-                                            SharedAlbumOptions = new SharedAlbumOptions()
-                                                                 {IsCollaborative = false, IsCommentable = false}
-                                        };
-
-                var sresp = serv.Albums.Share(sar, body2.AlbumId).Execute().Dump();
-
-                var addedPhotoUrls = new List<string>();
-
-
-                foreach (var imageFilename in imageFilenames)
-                {
-                    var imagePath = Path.Combine(Path.GetDirectoryName(postPath), imageFilename);
-                    var uploadId = UploadImage(imagePath, credential.Token.AccessToken);
-
-                    body2.NewMediaItems.Add(new NewMediaItem()
-                                            {
-                                                SimpleMediaItem = new SimpleMediaItem() {UploadToken = uploadId}
-                                            });
-                }
-
-
-                var addedPhoto = serv.MediaItems.BatchCreate(body2).Execute().Dump();
+                UploadPhotos(postPath, photosService, accessToken, postPathName, imageFilenames);
             }
 
-            var getFinalUrls = true;
-            if (getFinalUrls)
+            if (_getFinalUrls)
             {
                 var finalUrls = GetFinalUrls(postPathName);
-                int i = 0;
-                foreach (var imageF in imageFilenames)
+//                var i = 0;
+//              
+
+                if (changePostUrls)
                 {
-                    (finalUrls[i], imageF).Dump();
-                    i++;
+                    var i = 0;
+                    foreach (var imageFilename in imageFilenames)
+                    {
+                        var postText = File.ReadAllText(postPath);
+                        postText = postText.Replace(imageFilename, finalUrls[i]);
+                        File.WriteAllText(postPath,postText);
+                        i++;
+                    }
                 }
             }
-
-//	var urls = addedPhoto.NewMediaItemResults.Select(r => r.MediaItem.ProductUrl).ToArray();
-//	urls.Dump();
-
-
-            //	doOAuth();
         }
 
-
-        public string[] GetFinalUrls(string albumname)
+        private static void UploadPhotos(string postPath, PhotosLibraryService photosService, string accessToken,
+                                         string postPathName, string[] imageFilenames)
         {
-            var wcForAa = new WebClient();
-            wcForAa.Headers.Add("Cookie", authCookie);
-            var res = Encoding.UTF8.GetString(wcForAa.DownloadData(aaUrl));
+            var albumRq = new CreateAlbumRequest {Album = new Album {Title = postPathName}};
 
-            var match = Regex.Split(res, "\n").Where(l => l.Contains($",\"{albumname}\"")).Select(str => str.Trim(','))
-                             .FirstOrDefault();
+            var response = photosService.Albums.Create(albumRq).Execute().Dump();
 
-            var albumUrl = $"{aaUrl}/album/{match?.Split('"')[5]}";
 
-            res = Encoding.UTF8.GetString(wcForAa.DownloadData(albumUrl));
+            var photosRq = new BatchCreateMediaItemsRequest
+                           {
+                               AlbumId = response.Id,
+                               NewMediaItems = new List<NewMediaItem>()
+                           };
 
-            var lines = Regex.Split(res, "\n").Where(l => l.Contains($"googleusercontent") && l.Contains("=w"))
-                             .FirstOrDefault();
-            var results = new List<string>();
-            foreach (Match m in Regex.Matches(lines, @"img src=""([^""=]+)"))
+            var shareRq = new ShareAlbumRequest
+                          {
+                              SharedAlbumOptions = new SharedAlbumOptions
+                                                   {IsCollaborative = false, IsCommentable = false}
+                          };
+
+            var shareResp = photosService.Albums.Share(shareRq, photosRq.AlbumId).Execute().Dump();
+
+
+            foreach (var imageFilename in imageFilenames)
             {
-                results.Add(m.Groups[1].Value.Dump());
+                var imagePath = Path.Combine(Path.GetDirectoryName(postPath), imageFilename);
+                var uploadId = UploadImage(imagePath, accessToken);
+
+                photosRq.NewMediaItems.Add(new NewMediaItem
+                                           {
+                                               SimpleMediaItem = new SimpleMediaItem {UploadToken = uploadId}
+                                           });
             }
 
-            return results.ToArray();
+
+            var addedPhoto = photosService.MediaItems.BatchCreate(photosRq).Execute().Dump();
         }
 
-        public string UploadImage(string fullPath, string token)
+        private static string UploadImage(string fullPath, string token)
         {
-            FileStream fS = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
-            byte[] b = new byte[fS.Length];
+            var fS = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+            var b = new byte[fS.Length];
             fS.Read(b, 0, (int) fS.Length);
             fS.Close();
             var httpWebRequest = (HttpWebRequest) WebRequest.Create("https://photoslibrary.googleapis.com/v1/uploads");
@@ -160,59 +249,5 @@
             TextReader sr = new StreamReader(str);
             return sr.ReadToEnd();
         }
-
-        public string GetPermUrl(string photoSharingUrl)
-        {
-            var wc = new WebClient();
-            //wc.DownloadString();
-            var col = new NameValueCollection();
-            col.Add("googlephotos", photoSharingUrl);
-            wc.Headers.Add("Referer", "https://ctrlq.org/google/photos/");
-
-            var r = wc.UploadValues($"https://ctrlq.org/google/photos/og.php", col);
-            var json = Encoding.UTF8.GetString(r).Dump();
-            var res = (string) JsonConvert.DeserializeObject<dynamic>(json).image;
-            return res;
-        }
-
-        public string[] GetImagesInFile(string path)
-        {
-            var result = new List<string>();
-            var lines = File.ReadAllLines(path);
-            //while (Console.In.Peek() != -1)
-            {
-                //var line = Console.ReadLine();//
-                //var line="wfwf ![enter image description here](https://lh3.googleusercontent.com/jYdh5LNi5_3UzhCpMfx7NgCa3KeKklwsmmJ9bSPf4Tode4CowiVCjW80IMgSBhYgA4YV721slNUWCA) wfwf";
-                foreach (var line in lines)
-                {
-                    var pattern = @"\!\[[^]]*\]+\(([^)]+)\)";
-
-                    var matches = Regex.Match(line, pattern);
-                    using (WebClient wc = new WebClient())
-                    {
-                        if (matches.Success)
-                        {
-                            var val = matches.Groups[1].Value;
-                            result.Add(val);
-                        }
-                    }
-
-                    ;
-                }
-
-                return result.ToArray();
-            }
-        }
-
-        public const string clientId = "276048709422-3ov23ibvjjlv28v4f9hmsrbnb4l4igl7.apps.googleusercontent.com";
-        public const string clientSecret = "KKBt9jn6cqBQxqCOF2dTKXkl";
-        const string authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
-        const string tokenEndpoint = "https://www.googleapis.com/oauth2/v4/token";
-        const string userInfoEndpoint = "https://www.googleapis.com/oauth2/v3/userinfo";
-
-        const string aaUrl = "https://get.google.com/albumarchive/101096522944241604628";
-
-        const string authCookie =
-            "CONSENT=YES+LV.ru+20160410-02-0; S=billing-ui-v3=QWBYsxsPARdklJOhGWMbcxbZMr06-zUJ:billing-ui-v3-efe=QWBYsxsPARdklJOhGWMbcxbZMr06-zUJ; OTZ=5018887_44_48_123900_44_436380; SID=mgdAedstvVNuVCUPVRtSn1JpsfPNWz4oA_8ib-1oYh-bKtG3nhqaIGNAPGYNg5WW5_dAtA.; HSID=AIdPjjStOEoCL2EGL; SSID=A5tQLZmaHBfKhhaSA; APISID=H2tFYZEXFPocDCQP/A5zAu3Bri3ehTJ3Kk; SAPISID=wAVJ8HmKDqRlSzB8/Ah9TTFkt3MmQthi9F; SEARCH_SAMESITE=CgQIu40B; 1P_JAR=2019-7-30-15; NID=188=SjnoUPTJVg6mfp7YPt9_qUcIL6Ef5DK74qF0Q4n-8gb2Vz4maBTypmMCh7sVi4sWsu26s25kTIZFLGqGUbwEQByjhXMl4HRfo_vZcv_jzH53R5uK-4ccHsfvi9C6XpnyK-q96P_P6xvFfWeVAlhkq1snlSFVONnr1pnVLq__idmzCwIyZWsyeNBPD3bsNMPtXLx-kl_wUDtpOUsNq-RBjOjRouzBGOjkZcY1PAW8jU1LnQTc62A8Z9Fc12cP2pIt4eLHvUgWqXHSW6w5iMk6t5n79QxPn7ce5OMG34vsJvl1cpDwycR_52Lm8MNRmdXTybdtl5MM7Gpw2cByhnUF2-oYpo8JGQ; SIDCC=AN0-TYsvt11riFowOC9ifjxslSsjZFNF2o0kx6MSLrNxN4gSC1NIFfezMc_Od7SJocv2FZNdpRM";
     }
 }

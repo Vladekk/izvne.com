b4c2dbce04c4cebbacc58724b482282881873ec1
vkugelevics <vladislavs.kugelevics@28stone.com>
2019-08-18 15:16:50 +0300

    Trying to switch to global dotnet and wyam installs

--- Izvne.com/build.sh
+++ Izvne.com/build.sh
@@ -1,13 +1,14 @@
-wget https://dot.net/v1/dotnet-install.sh
-chmod +x ./dotnet-install.sh
-./dotnet-install.sh -c Current
-export PATH="$PATH:/opt/buildhome/.dotnet/tools"
-export PATH="$PATH:/opt/buildhome/.dotnet"
-export DOTNET_ROOT=/opt/buildhome/.dotnet
-#dotnet tool install -g Wyam.Tool	
+#wget https://dot.net/v1/dotnet-install.sh
+#chmod +x ./dotnet-install.sh
+#./dotnet-install.sh -c Current
+#export PATH="$PATH:$HOME/.dotnet/tools"
+#export PATH="$PATH:$HOME/.dotnet"
+#export DOTNET_ROOT=$HOME/.dotnet
+dotnet tool install -g Wyam.Tool	
 
 cd Izvne.com
-dotnet ./lib/Wyam.dll build config.wyam --use-local-packages -o ../wwwroot
+#dotnet ./lib/Wyam.dll build config.wyam --use-local-packages -o ../wwwroot
+wyam build config.wyam -o ../wwwroot
 
 
 if [ $? -ne 0 ]; then
--- Izvne.com/local.bat
+++ Izvne.com/local.bat
@@ -1,4 +1,5 @@
 :start
-call dotnet .\lib\Wyam.dll  build --use-local-packages -w --noclean  -o ..\wwwroot -c local.wyam
+#call dotnet .\lib\Wyam.dll  build --use-local-packages -w --noclean  -o ..\wwwroot -c local.wyam
+Wyam  build  -w --noclean  -o ..\wwwroot -c local.wyam
 rem mklink /j ..\wwwroot\files ..\files
 rem pause
